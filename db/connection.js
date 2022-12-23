5d47796c5c90cb06941fae446ecf2cba70d615ec
sdmantha <sdangelcake@gmail.com>
2022-12-23 13:51:44 -0500

    changed the mongoose.connect and added a const url in the connection.js file

--- db/connection.js
+++ db/connection.js
@@ -3,7 +3,10 @@ import chalk from "chalk";
 
 mongoose.set("returnOriginal", false);
 
-mongoose.connect("mongodb://127.0.0.1:27017/fish-api").catch((err) => {
+const url = process.env.MONGO_URL || 'mongodb://127.0.0.1:27017/fish-api'
+
+
+mongoose.connect(url).catch((err) => {
   console.log(`Error connection to MongoDB: ${err.message}`);
 });
 
